e8da526b07c97e015645211fafc6305d5e989dc9
JamesHiro <unicornjameser@gmail.com>
2022-09-17 21:21:33 +0900

    add partners logo

--- src/components/PartnerCard.tsx
+++ src/components/PartnerCard.tsx
@@ -10,7 +10,7 @@ export const PartnerCard = (props: PartnerCardProps) => {
     return (
         <div className="box border p-4 md:p-6 transition ease-in-out delay-150 hover:-translate-y-2 duration-300">
             <div className="bg-cover">
-                {/* <img className="w-4/6 mb-5" src={card.logo}></img> */}
+                <img className="w-4/6 mb-5" src={card.logo}></img>
             </div>
             <h4 className="mb-5 text-stone-700">{card.title}</h4>
             <p className="text-gray-400">
