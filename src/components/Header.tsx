bc3ee255f271acd551c4aedc629ab5b07e22406c
JamesHiro <unicornjameser@gmail.com>
2022-09-18 15:07:25 +0900

    disable basic CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./component.css";
 import { useMediaQuery } from 'react-responsive'
 
 const Header = () => {
