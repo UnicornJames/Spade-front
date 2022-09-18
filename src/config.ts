647d2d8e87fc7f3aa8975ad13260590dd6d1df5a
JamesHiro <unicornjameser@gmail.com>
2022-09-19 08:22:32 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // ======================= Real work mod =============================//
-// export const API_URL = "https://new-reserve.herokuapp.com";
+export const API_URL = "https://new-reserve.herokuapp.com";
 
 // ====================== Development mod ============================//
-export const API_URL = "localhost:3000";
+// export const API_URL = "localhost:3000";
