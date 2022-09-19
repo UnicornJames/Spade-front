f2f4715a9bafa435d611882cddb68a090a390370
JamesHiro <unicornjameser@gmail.com>
2022-09-19 14:29:49 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // ======================= Real work mod =============================//
-export const API_URL = "https://new-reserve.herokuapp.com";
-// export const API_URL = "https://spade-test-back.herokuapp.com/";
+// export const API_URL = "https://new-reserve.herokuapp.com";
+export const API_URL = "https://spade-test-back.herokuapp.com";
 
 // ====================== Development mod ============================//
 // export const API_URL = "localhost:3000";
