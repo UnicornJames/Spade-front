07741f1d75e9a4e8fe7c231b89ffb0cbe29db2d7
JamesHiro <unicornjameser@gmail.com>
2022-09-20 16:25:13 +0900

    add chart

---
+++ src/components/Chart.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { IgrFinancialChart } from "igniteui-react-charts";
+import { IgrFinancialChartModule } from "igniteui-react-charts";
+import StocksHistory from "./StocksHistory";
+
+IgrFinancialChartModule.register();
+
+export default class Chart extends React.Component<any, any> {
+  public data: any[] | undefined;
+
+  constructor(props: any) {
+    super(props);
+    this.state = { data: [] };
+    this.initData();
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div className="container sample">
+        <div className="container">
+          <IgrFinancialChart
+            width="100%"
+            height="500px"
+            chartType="Line"
+            dataSource={this.state.data}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  public initData() {
+    StocksHistory.getMultipleStocks().then((stocks: any[]) => {
+      this.setState({ data: stocks });
+    });
+  }
+}
