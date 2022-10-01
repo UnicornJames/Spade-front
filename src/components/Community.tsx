47784f12d0468c6383844184cfde8cc385b3343b
JamesHiro <unicornjameser@gmail.com>
2022-10-01 15:42:40 +0900

    font style

--- src/components/Community.tsx
+++ src/components/Community.tsx
@@ -39,11 +39,11 @@ const Community: React.FC<CommunityProps> = () => {
       {/* <Tabs tabs={tabs} /> */}
 
       <div className="flex justify-start mb-8">
-        <button data-target="#demo" onClick={() => setActived(0)} data-slide-to="0" className={`px-2 text-xl ${actived == 0 ? 'border-[#11DD33] text-[#11DD33]': 'text-[#222222aa]'} border-b-2 hover:text-[#11DD33]`}><p className="text-sm md:text-base">Supply</p></button>
-        <button data-target="#demo" onClick={() => setActived(1)} data-slide-to="1" className={`px-2 text-xl ${actived == 1 ? 'border-[#11DD33] text-[#11DD33]': 'text-[#222222aa]'} border-b-2 hover:text-[#11DD33]`}><p className="text-sm md:text-base">Borrow</p></button>
-        <button data-target="#demo" onClick={() => setActived(2)} data-slide-to="2" className={`px-2 text-xl ${actived == 2 ? 'border-[#11DD33] text-[#11DD33]': 'text-[#222222aa]'} border-b-2 hover:text-[#11DD33]`}><p className="text-sm md:text-base">Control</p></button>
-        <button data-target="#demo" onClick={() => setActived(3)} data-slide-to="3" className={`px-2 text-xl ${actived == 3 ? 'border-[#11DD33] text-[#11DD33]': 'text-[#222222aa]'} border-b-2 hover:text-[#11DD33]`}><p className="text-sm md:text-base">Audit</p></button>
-        <button data-target="#demo" onClick={() => setActived(4)} data-slide-to="4" className={`px-2 text-xl ${actived == 4 ? 'border-[#11DD33] text-[#11DD33]': 'text-[#222222aa]'} border-b-2 hover:text-[#11DD33]`}><p className="text-sm md:text-base">Insurance</p></button>
+        <button data-target="#demo" onClick={() => setActived(0)} data-slide-to="0" className={`px-2 text-xl ${actived == 0 ? 'border-[#00D395] text-[#00D395]': 'text-[#CCD6DD]'} border-b-2 hover:text-[#00D395]`}><p className="text-sm md:text-xl font-medium">Supply</p></button>
+        <button data-target="#demo" onClick={() => setActived(1)} data-slide-to="1" className={`px-2 text-xl ${actived == 1 ? 'border-[#00D395] text-[#00D395]': 'text-[#CCD6DD]'} border-b-2 hover:text-[#00D395]`}><p className="text-sm md:text-xl font-medium">Borrow</p></button>
+        <button data-target="#demo" onClick={() => setActived(2)} data-slide-to="2" className={`px-2 text-xl ${actived == 2 ? 'border-[#00D395] text-[#00D395]': 'text-[#CCD6DD]'} border-b-2 hover:text-[#00D395]`}><p className="text-sm md:text-xl font-medium">Control</p></button>
+        <button data-target="#demo" onClick={() => setActived(3)} data-slide-to="3" className={`px-2 text-xl ${actived == 3 ? 'border-[#00D395] text-[#00D395]': 'text-[#CCD6DD]'} border-b-2 hover:text-[#00D395]`}><p className="text-sm md:text-xl font-medium">Audit</p></button>
+        <button data-target="#demo" onClick={() => setActived(4)} data-slide-to="4" className={`px-2 text-xl ${actived == 4 ? 'border-[#00D395] text-[#00D395]': 'text-[#CCD6DD]'} border-b-2 hover:text-[#00D395]`}><p className="text-sm md:text-xl font-medium">Insurance</p></button>
       </div>
 
       <div id="demo" className="carousel slide" data-ride="carousel">
--- src/components/tab_subcomponents/CommunityCard.tsx
+++ src/components/tab_subcomponents/CommunityCard.tsx
@@ -9,7 +9,7 @@ const CommunityCard = (props: CommunityCardProps) => {
   const { card } = props;
 
   return (
-    <div className="box hover:shadow-xl p-2 md:p-6 transition ease-in-out delay-150 hover:-translate-y-2 duration-300">
+    <div className="box hover:shadow-4xl shadow-xl p-2 md:p-6 transition ease-in-out delay-150 hover:-translate-y-2 duration-300 cursor-pointer">
       <div className="bg-cover">
         <img className="w-11/12 md:4/6 mb-5" src={card.logo}></img>
       </div>
@@ -19,7 +19,7 @@ const CommunityCard = (props: CommunityCardProps) => {
       <p className="text-gray-400 text-center md:text-left">
         {card.description}
       </p>
-      <div className="flex justify-between text-green hidden hover:block">
+      <div className="flex justify-between text-green hidden hover:">
         <a>Try</a>
         <span id="arrow" className="arrow"></span>
       </div>
