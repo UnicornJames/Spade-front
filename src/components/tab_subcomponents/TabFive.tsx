24d59f9b729f4e97dec91ebe6341574efbd89661
JamesHiro <unicornjameser@gmail.com>
2022-10-02 13:07:09 +0900

    Revert "community cards routing fix"
    This reverts commit f591e5c91a199b03dd2c335896d024e7956f1d15.

--- src/components/tab_subcomponents/TabFive.tsx
+++ src/components/tab_subcomponents/TabFive.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import CommunityCard from "./CommunityCard"
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 
-interface TabProps {
-  responsive: any
-}
-
-const TabFive:React.FC<TabProps> = ( {responsive} ) => {
+const TabFive = () => {
   const exploreList = [
     { title: "Ledger", logo: "logos/ledger.svg", description: "Easily earn interest & invest; securely store & send. No seed phrase. No gas."},
     { title: "Coinbase Wallet", logo: "logos/coinbase.svg", description: "Earn interest and borrow assets on one of the most popular crypto exchanges."},
@@ -17,19 +11,10 @@ const TabFive:React.FC<TabProps> = ( {responsive} ) => {
     { title: "Gnosis", logo: "logos/gnosis.svg", description: "Fully-featured crypto wallet mobile app, integrating Compound."},
   ]
   return (
-    <div className="">
-      <div className="lg:hidden grid gap-4 py-2">
-        <Carousel responsive={responsive}>
-          {exploreList.map((item, i) => (
-            <CommunityCard key={i} card={item} />
-          ))}
-        </Carousel>
-      </div>
-      <div className="hidden lg:grid grid-cols-3 xl:grid-cols-4 grid-col-1 gap-4">
-        {exploreList.map((item, i) => (
-          <CommunityCard key={i} card={item} />
-        ))}
-      </div>
+    <div className='grid md:grid-cols-3 grid-col-1 gap-4'>
+      {exploreList.map ((item, i) => (
+        <CommunityCard key = {i} card={item} />
+      ))}
     </div>
   )
 }
