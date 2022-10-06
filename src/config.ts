21eebd07415bc67bdbf74f0df730c69748ebc2eb
James <104578379+UnicornJames@users.noreply.github.com>
2022-10-06 17:39:29 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // ========================== Test mod ===============================//
-export const API_URL = "https://agile-lake-30539.herokuapp.com/";
+export const API_URL = "https://agile-lake-30539.herokuapp.com";
 
 // ====================== Development mod ============================//
 // export const API_URL = "localhost:3000";
