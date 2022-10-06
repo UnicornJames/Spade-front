876dc3d3755d41d0da048e33f1c7784324d301c5
JamesHiro <unicornjameser@gmail.com>
2022-10-06 17:09:28 +0900

    fix mongo url

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,5 @@
-// ========================== Old mod ================================//
-// export const API_URL = "https://cors-anywhere.herokuapp.com/reserve-api-withspade.herokuapp.com";
-
-// ======================= Real work mod =============================//
-// export const API_URL = "https://spadeoracleserver.herokuapp.com";
-
 // ========================== Test mod ===============================//
-export const API_URL = "https://spadeoracleserver.herokuapp.com";
+export const API_URL = "https://agile-lake-30539.herokuapp.com/";
 
 // ====================== Development mod ============================//
 // export const API_URL = "localhost:3000";
